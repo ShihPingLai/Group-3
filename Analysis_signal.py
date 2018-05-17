fbaeb42079d488bced06d0aec41fff63e31ada91
ShiauJie <38398559+ShiauJie@users.noreply.github.com>
2018-05-17 14:39:30 +0800

    新的解密，解出聲音波形

--- Analysis_signal.py
+++ Analysis_signal.py
@@ -3,15 +3,20 @@ import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-o = open("in.txt","r")
+o1 = open("in1.txt","r")
+o2 = open("in2.txt","r")
+ot = open("time.txt","r")
+SigMix1=[]
+SigMix2=[]
+t=[]
 
-SigMix=[]
+for i in range(720832):
+    SigMix1.append(float(o1.readline()))
+    SigMix2.append(float(o2.readline()))
+    t.append(float(ot.readline()))
 
-for i in range(1000):
-    SigMix.append(float(o.readline()))
 
 
-t = np.linspace(0, 30, 1000)
 """
 plt.plot(t,SigMix)
 plt.show()
@@ -42,7 +47,13 @@ H, infodict = integrate.odeint(dH_dt, H0, t, full_output=True)
 
 #SigMix-H
 
-out=SigMix-H[:,0]
-plt.plot(t,out)
+out1=SigMix1-H[:,0]*5000
+out2=SigMix2-H[:,1]*5000
+fig1=plt.figure(1)
+plt.plot(t,out1)
+plt.show()
+
+fig2=plt.figure(2)
+plt.plot(t,out2)
 plt.show()
 
